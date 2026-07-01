f19799d4e97d4a2d10a282c8b79870909e43f769
shamEiNew <panchalshamsundar7@gmail.com>
2026-07-01 22:34:54 +0530

    tactics update

--- LeanMath/tpil4/tactis.lean
+++ LeanMath/tpil4/tactis.lean
@@ -56,3 +56,40 @@ example : ∀ a b c : Nat , a = b → a = c → c = b := by unhygienic
 
 example : ∀ a b c : Nat , a = b → a = c → c = b :=
   (fun a b c => fun h1 : a = b => fun h2 : a = c => Eq.trans (Eq.symm h2) h1)
+
+example (p q : α → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro ⟨w, hpw, hqw⟩
+  exact ⟨w, hqw, hpw⟩
+
+example (p q : α → Prop) : (∃ x, p x ∨ q x) → ∃ x, q x ∨ p x := by
+  intro
+  | ⟨w, Or.inl h⟩ => exact ⟨w, Or.inr h⟩
+  | ⟨w, Or.inr h⟩ => exact ⟨w, Or.inl h⟩
+
+theorem test1 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  case left => exact hp
+  case right =>
+    apply And.intro
+    case left => exact hq
+    case right => exact hp
+
+
+example : ∀ a b c d : Nat, a = b → a = d → a = c → c = b := by
+  intros
+  rename_i h1 _ h2
+  apply Eq.trans
+  apply Eq.symm
+  exact h2
+  exact h1
+
+example (x : Nat) : x = x := by
+  revert x
+  intro y
+  rfl
+
+example (x y : Nat) (h : x = y) : y = x := by
+  revert x
+  intros
+  apply Eq.symm
+  assumption
