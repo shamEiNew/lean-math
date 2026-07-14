37afa665aa2cfab931723e23cf27aea17b43de6f
shamEiNew <panchalshamsundar7@gmail.com>
2026-07-14 22:27:05 +0530

    inductive types 2

--- LeanMath/tpil4/inductive.lean
+++ LeanMath/tpil4/inductive.lean
@@ -92,17 +92,22 @@ end Weekday
 Constructors with Arguments.
 The argument motive in `prod_example`is used to specify the type of the object you want to construct,
 and it is a function because it may depend on the pair.
+that a type with multiple constructors is disjunctive: an element of `Sum α β`
+is either of the form `inl a` or of the form `inl b`.
 -/
 
 namespace Hidden
+
+-- Single Constructor with multiple arguments : Conjunctive
 inductive Prod (α : Type u) (β : Type v)
   | mk : α → β → Prod α β
 
+-- Multiple constructors ``inl` and `inr`: Disjunctive
 inductive Sum (α : Type u) (β : Type v) where
   | inl : α → Sum α β
   | inr : β → Sum α β
 
-#check Prod
+#check Prod.casesOn
 
 --Extracts the first element from (a, b)
 def fst_element (p : Prod α β):α :=
@@ -122,4 +127,67 @@ def prod_example (p : Prod Bool Nat) : Nat :=
 
 #check (true, 3)
 #check Prod.mk true 3
+
+#check Sum.casesOn
+
+def sum_example (s : Sum Nat Nat) : Nat :=
+  Sum.casesOn (motive := fun _ => Nat) s
+    (fun n => 2 * n)
+    (fun n => 2 * n + 1)
+
+--Note that motive is implict and can be infered from the types
+def sum_example_1 (s : Sum Nat Nat) : Nat :=
+  Sum.casesOn s
+    (fun n => 2 * n)
+    (fun n => 2 * n + 1)
+
+#eval sum_example (Sum.inl 3)
+#eval sum_example_1 (Sum.inr 3)
+
+/-
+An arbitrary inductive type can include both features, by having any number of constructors,
+each of which takes any number of arguments.
+-/
+inductive Prod1 (α : Type u) (β : Type v) where
+  | mk (fst : α) (snd : β) : Prod1 α β
+
+#check Prod1.mk
+/-
+structures are special case of inductive types as it
+allows to construct named arguments and infers the constructor
+-/
+
+structure Prod2 (α : Type u) (β : Type v) where
+  mk ::
+  fst : α
+  snd : β
+/-
+If we don't name the constructor lean default name is mk
+-/
+
+structure Prod3 (α : Type u) (β : Type v) where
+  mb ::
+  fst : α
+  snd : β
+
+#check Prod3.mb
+#check Prod3.casesOn
+
+#check Nat.casesOn
+#check Sum.casesOn
+
+--Example of Nat as in above using motive.
+-- But note that casesOn differs here slightly
+-- as in inductive type `zero:Nat` has no arguments.
+def nat_to_bool (n : Nat) : Nat :=
+    Nat.casesOn (motive := fun _ => Nat) n
+      (6)
+      (fun n => n)
+
+--Note that when we eval the above argument passed to
+-- function is the argument of succ which is the previous natural
+-- to increment we want `n+2` and to get the same `n+1`
+#eval nat_to_bool 3
+#eval nat_to_bool 0
+
 end Hidden
